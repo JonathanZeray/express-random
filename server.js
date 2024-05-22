632e0abbfbd03f6f5750935edc540db7f213ea60
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-05-22 13:31:30 +0200

    add route/query params,  statuses in response

--- server.js
+++ server.js
@@ -15,12 +15,24 @@ let posts = [
 ];
 // Get all posts
 app.get("/api/posts", (req, res) => {
-  res.json(posts);
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  res.status(200).json(posts);
 });
 
 //Get single post
-app.get("/api/posts", (req, res) => {
-  res.json(posts);
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ msg: `a post with the id of ${id} was not found` });
+  }
+  res.status(200).json(post);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
