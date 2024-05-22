d11229f998d94c9d6f0b62eeba6f1e39a94b9691
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-05-22 15:41:56 +0200

    updated error handlers

--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,7 @@ let posts = [
 ];
 
 // Get all posts
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
   const limit = parseInt(req.query.limit);
   if (!isNaN(limit) && limit > 0) {
     return res.status(200).json(posts.slice(0, limit));
@@ -30,42 +30,44 @@ router.get("/:id", (req, res, next) => {
 });
 
 //Create new post
-router.post("/", (req, res) => {
+router.post("/", (req, res, next) => {
   const newPost = {
     id: posts.length + 1,
     title: req.body.title,
   };
 
   if (!newPost.title) {
-    return res.status(400).json({ msg: "Please include a title" });
+    const error = new Error(`Please include a title`);
+    error.status = 400;
+    return next(error);
   }
   posts.push(newPost);
   res.status(201).json(posts);
 });
 
 //Update a post
-router.put("/:id", (req, res) => {
+router.put("/:id", (req, res, next) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
 
   if (!post) {
-    return res
-      .status(404)
-      .json({ msg: `a post with the id of ${id} was not found` });
+    const error = new Error(`a post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
   }
   post.title = req.body.title;
   res.status(200).json(posts);
 });
 
 //Delete a post
-router.delete("/:id", (req, res) => {
+router.delete("/:id", (req, res, next) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
 
   if (!post) {
-    return res
-      .status(404)
-      .json({ msg: `a post with the id of ${id} was not found` });
+    const error = new Error(`a post with the id of ${id} was not found`);
+    error.status = 404;
+    return next(error);
   }
   posts = posts.filter((post) => post.id !== id);
   res.status(200).json(posts);
