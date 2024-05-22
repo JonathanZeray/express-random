132ea1ee86cd46b6a2eb30f7263908c6c23a547a
Jonathan Zeray <jonathan.zeray@hotmail.com>
2024-05-22 13:03:53 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+//setup static folder
+//app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  { id: 1, title: "Post one" },
+  { id: 2, title: "Post two" },
+  { id: 3, title: "Post three" },
+];
+// Get all posts
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+//Get single post
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
